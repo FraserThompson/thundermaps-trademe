117a291e22db2caf8c21138aaa0c87c1976d445c
Daniel Gibbs <daniel@danielgibbs.name>
2013-11-29 11:43:51 +1300

    Added updater module.

--- trademe.py
+++ trademe.py
@@ -14,66 +14,60 @@ import json
 import time
 import datetime
 
-# Internal requests object used to perform requests.
-# Is replaced by an OAuth1Session object when authenticated.
-trademe = requests
-
-# Whether to print debug information.
-debug = False
-
-# TradeMe category numbers.
-RENTAL_CATEGORY = 4233
-
-# Authenticate with the TradeMe API.
-def authenticate(consumer_key, consumer_secret, oauth_token, oauth_secret):
-	global trademe
-	trademe = requests_oauthlib.OAuth1Session(consumer_key, client_secret=consumer_secret, resource_owner_key=oauth_token, resource_owner_secret=oauth_secret)
-
-# Unauthenticate from the TradeMe API.
-def unauthenticate():
-	global trademe
+class TradeMe:
+	# Internal requests object used to perform requests.
+	# Is replaced by an OAuth1Session object when authenticated.
 	trademe = requests
 
-# Set debug mode.
-def debug(on=True):
-	global debug
-	debug = on
+	# Whether to print debug information.
+	debug = False
 
-# Get a list of rentals from TradeMe.
-def getRentals(limit=25, since=time.time()-86400):
-	global trademe
-	more = True
-	page = None
-	count = 0
-	rentals = []
+	# Authenticate with the TradeMe API.
+	def authenticate(self, consumer_key, consumer_secret, oauth_token, oauth_secret):
+		self.trademe = requests_oauthlib.OAuth1Session(consumer_key, client_secret=consumer_secret, resource_owner_key=oauth_token, resource_owner_secret=oauth_secret)
 
-	# While there are more listings to get, get them.
-	while more:	
-		# Build URL.
-		url = "http://api.trademe.co.nz/v1/Search/General.json"
-		params = {"category": RENTAL_CATEGORY}
-		params["rows"] = 500 if limit == None else limit - count
-		if since != None:
-			params["date_from"] = datetime.datetime.utcfromtimestamp(since).strftime('%Y-%m-%dT%H:%M:%SZ')
-		if page != None:
-			params["page"] = page
-		# Print debug information in debug mode.
-		if debug == True:
-			print "trademe: [url=%s, params=%s]" % (url, params)
-		# Peform the request.
-		try:
-			resp = trademe.get(url, params=params)
-			result = resp.json()
-		except Exception as e:
-			print "Error retrieving rentals: %s" % e
-			return rentals
-		# Add each rental to the result.
-		for rental in result["List"]:
-			rentals.append(rental)
-			count = count + 1
-		# Get next page if there are more listings.
-		if (count >= result["TotalCount"]) or (limit != None and count >= limit):
-			more = None
-		else:
-			page = result["Page"] + 1
-	return rentals
+	# Unauthenticate from the TradeMe API.
+	def unauthenticate(self):
+		self.trademe = requests
+
+	# Set debug mode.
+	def debug(self, on=True):
+		self.debug = on
+
+	# Get a list of listings from TradeMe.
+	def getListings(self, category_id, limit=25, since=time.time()-86400):
+		more = True
+		page = None
+		count = 0
+		listings = []
+
+		# While there are more listings to get, get them.
+		while more:
+			# Build URL.
+			url = "http://api.trademe.co.nz/v1/Search/General.json"
+			params = {"category": category_id}
+			params["rows"] = 500 if limit == None else limit - count
+			if since != None:
+				params["date_from"] = datetime.datetime.utcfromtimestamp(since).strftime('%Y-%m-%dT%H:%M:%SZ')
+			if page != None:
+				params["page"] = page
+			# Print debug information in debug mode.
+			if self.debug == True:
+				print "trademe: [url=%s, params=%s]" % (url, params)
+			# Peform the request.
+			try:
+				resp = self.trademe.get(url, params=params)
+				result = resp.json()
+			except Exception as e:
+				print "Error retrieving listings: %s" % e
+				return listings
+			# Add each rental to the result.
+			for rental in result["List"]:
+				listings.append(rental)
+				count = count + 1
+			# Get next page if there are more listings.
+			if (count >= result["TotalCount"]) or (limit != None and count >= limit):
+				more = None
+			else:
+				page = result["Page"] + 1
+		return listings
---
+++ updater.py
@@ -0,0 +1,81 @@
+import thundermaps
+import trademe
+import time
+
+class Updater:
+	categories = {}
+
+	def __init__(self, key):
+		self.thundermaps_api_key = key
+		self.trademe = trademe.TradeMe()
+
+	def authenticate(self, key, secret, oauth_key, oauth_secret):
+		self.trademe.authenticate(key, secret, oauth_key, oauth_secret)
+
+	def add_category(self, name, trademe_id, account_id, thundermaps_id):
+		# Add the category.
+		self.categories[name] = {
+			"trademe_id": trademe_id,
+			"account_id": account_id,
+			"thundermaps_id": thundermaps_id,
+			"since": int(time.time())
+		}
+
+		# Try to load the last update time from file.
+		try:
+			update_file = open(".lastupdate_" + name, "r")
+			self.categories[name]["since"] = int(update_file.read())
+			update_file.close()
+		except Exception as e:
+			print "* No valid timestamp file was found for '%s': defaulting to now." % name
+
+	# Start updating the categories.
+	def start(self):
+		# Run until interrupt received.
+		while True:
+			for category_name in self.categories:
+				category = self.categories[category_name]
+
+				# Get listings from TradeMe.
+				listings = self.trademe.getListings(category_id=category["trademe_id"], limit=None, since=category["since"])
+
+				# Update timestamp of last update.
+				category["since"] = time.time()
+
+				# Create reports for the listings.
+				reports = []
+				for listing in listings:
+					# Some listings don't have geographic information: ignore these.
+					if "GeographicLocation" not in listing.keys():
+						continue
+					# Include the auction URL in the description.
+					listing_url = "http://www.trademe.co.nz%s/auction-%d.html" % (listing["CategoryPath"], listing["ListingId"])
+					report = {
+						"latitude": listing["GeographicLocation"]["Latitude"],
+						"longitude": listing["GeographicLocation"]["Longitude"],
+						"category_id": category["thundermaps_id"],
+						"occurred_on": time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime(float(listing["StartDate"][6:-2])/1000)),
+						"description": "%s. See more at %s" % (("%s - %s" % (listing["Address"], listing["Title"]), listing_url) if "Address" in listing.keys() else (listing["Title"], listing_url)),
+						"source_id": "%d" % listing["ListingId"]
+					}
+					reports.append(report)
+				print "Retrieved %d reports for '%s'..." % (len(reports), category_name)
+
+				# If there is at least one report, send the reports to Thundermaps.
+				if len(reports) > 0:
+					thundermaps.sendReports(self.thundermaps_api_key, category["account_id"], reports)
+					print "Submitted %d reports..." % len(reports)
+
+				# Save the timestamp of the last update.
+				try:
+					update_file = open(".lastupdate_" + category_name, "w")
+					update_file.write("%d" % category["since"])
+					update_file.close()
+				except Exception as e:
+					print "! WARNING: Unable to write timestamp file for '%s'." % category_name
+					print "! If there is an old timestamp file when this script is next run, it may be using an old timestamp resulting in duplicate reports."
+					print "! To avoid this, delete the file '.lastupdate_%s'." % category_name
+
+			# Wait half an hour before trying again.
+			time.sleep(60 * 30)
+
